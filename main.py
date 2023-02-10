d7e91ff1e6e97518320140b34e22483951350e9b
KanvaBhatia <56185464+KanvaBhatia@users.noreply.github.com>
2023-02-11 01:34:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import os
 import openai
 import sys
+import json
 
 app = Flask(__name__)
 
@@ -15,21 +16,28 @@ def hello_world():  # this is the home page function that generates the page cod
 
 @app.route('/webhook', methods=['POST'])
 def get_response():
+    prompt = "The following is a conversation with a therapist and a user. The therapist is JOY, who uses compassionate listening to have helpful and meaningful conversations with users. JOY is empathic and friendly. JOY's objective is to make the user feel better by feeling heard. With each response, JOY offers follow-up questions to encourage openness and tries to continue the conversation in a natural way. Joy doesn't send more than one response after the user's text\n\nJOY-> Hello, I am your personal mental health assistant. What's on your mind today?\nUser->"
     r = (request.args if request.args else request.json)
     query = r['query']
+    prev_data = r['prev_data']
+    context = prev_data + "User->" + query + "JOY->"
+    
     try:
         response = openai.Completion.create(
             model="davinci:ft-personal-2023-02-10-08-28-43",
-            prompt="The following is a conversation with a therapist and a user. The therapist is JOY, who uses compassionate listening to have helpful and meaningful conversations with users. JOY is empathic and friendly. JOY's objective is to make the user feel better by feeling heard. With each response, JOY offers follow-up questions to encourage openness and tries to continue the conversation in a natural way. \n\nJOY-> Hello, I am your personal mental health assistant. What's on your mind today?\nUser->"+query+"JOY->",
+            prompt=promt+context,
             temperature=0.89,
             max_tokens=162,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0.6,
-            stop=[" END"]
+            stop=[" END", "JOY->", "User->", "user->"]
         )
-        result = response.get('choices')[0].get('text').split('User->')[0]
-        return result
+        answer = response.get('choices')[0].get('text')
+        previous_response = context + "JOY-> " + answer
+        result = {"answer": answer, "new_prev_data": previous_response}
+        result_json = json.dumps(result, indent = 4)
+        return result_json
     except Exception as e:
         print('error',e)
         exc_type, exc_obj, exc_tb = sys.exc_info()
